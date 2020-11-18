e115fa83a10ed0075fa941030bb8fe89cf5df4c5
Alice <carrizoalice@gmail.com>
2020-11-18 00:10:35 -0300

    update

--- src/main/java/com/amc/apirestfull/controller/NotaController.java
+++ src/main/java/com/amc/apirestfull/controller/NotaController.java
@@ -2,6 +2,7 @@ package com.amc.apirestfull.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +22,10 @@ public class NotaController {
 	@PutMapping("/nota")
 	public boolean agregarNota(@RequestBody Nota nota) {
 		return service.crear(nota);
+	}
+	
+	@PostMapping("/nota")
+	public boolean actualizarNota(@RequestBody Nota nota){
+		return service.actualizar(nota);
 	}
 }
